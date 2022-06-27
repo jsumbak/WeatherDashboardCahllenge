0e4dd5952105f4f1564fd2809cfcc8f58fa8c86c
jsumbak <julsumba311@gmail.com>
2022-06-27 15:59:30 -0700

    content added to JS during tutor session

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # WeatherDashboardCahllenge
 
-Live URL
+Live URL: https://jsumbak.github.io/WeatherDashboardCahllenge/
 
 Screenshot
 <img src="" alt="screenshot" >
--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,57 @@
 //create local storage
-$(".nine").val(localStorage.getItem("nine"))
+var cities = document.querySelector('ul');
+var fetchButton = document.getElementById('fetch-button');
+var apiKey = "5abd73aeb1b3de420209d0778a1f8ae9";
+var citySelection = document.querySelector("#city")
+var userFormEl = document.querySelector('.submitbtn');
+var currentWeather = document.querySelector("#today-forecast")
+
+console.log("hello")
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+
+    var cityName = citySelection.value.trim();
+
+    if (cityName) {
+        getApi(cityName);
+
+        citySelection.textContent = '';
+    } else {
+        alert('Please enter a valid city name');
+    }
+};
+
+
+function getApi(city) {
+    var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=5abd73aeb1b3de420209d0778a1f8ae9";
+
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log(data)
+
+                    var apiUrl2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data[0].lat + "&lon=" + data[0].lon + "&appid=5abd73aeb1b3de420209d0778a1f8ae9&units=imperial"
+                    fetch(apiUrl2).then(function (weatherdata) {
+                        weatherdata.json().then(function (data) { console.log(data.current)
+                            currentWeather.innerHTML = ""
+                            var cityName = document.createElement("p")
+                            cityName.innerText = city + " " + moment().format("L")
+                            currentWeather.appendChild(cityName)
+                            var temp = document.createElement("p")
+                            temp.innerText = data.current.temp 
+                            currentWeather.appendChild(temp)
+                           
+                        })
+                    })
+                });
+            } else {
+                alert('Error: ' + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            alert('Unable to connect to API');
+        });
+};
+
+userFormEl.addEventListener('click', formSubmitHandler);
